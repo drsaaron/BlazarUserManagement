450f0daec3080cb27a3c94d9fd5516cf3a649d10
Scott Aaron <dr_saaron@yahoo.com>
2023-07-04 13:51:33 -0500

    improve runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -5,6 +5,17 @@ imageName=$(dockerImageName.sh)
 imageVersion=$(getPomAttribute.sh version | sed 's/-RELEASE$//')
 containerName=$(echo $imageName | sed -re 's%^.*/([a-zA-Z]*)$%\1%') # could also use basename $(dockerImageName.sh)
 
+port=$(grep ^server.port src/main/resources/prod.properties | awk -F= '{ print $2 }')
+
 docker stop $containerName
 docker rm $containerName
-docker run --user $(id -u):$(id -g) --network qotd --name $containerName -d -p 4500:4500 -v ~/.blazartech:/home/$(whoami)/.blazartech $imageName:$imageVersion
+docker run \
+       --user $(id -u):$(id -g) \
+       --network qotd \
+       --name $containerName \
+       -d \
+       -p $port:$port \
+       -v ~/.blazartech:/home/$(whoami)/.blazartech \
+       --health-cmd="curl --silent --fail http://localhost:$port/monitoring/health || exit 1" \
+       --health-interval=60s \
+       $imageName:$imageVersion
